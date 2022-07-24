56d7ad90f69938201de418bf7cd29d0580fc7fd0
coolcty <coolcty@qq.com>
2022-07-24 20:54:45 +0200

    update

--- includes/ft.h
+++ includes/ft.h
@@ -5,6 +5,16 @@
 #ifndef FT_H
 # define FT_H
 
+
+struct nlist
+{                       /* table entry: */
+    struct nlist *next; /* next entry in chain */
+    char *name;         /* defined name */
+    char *defn;         /* replacement text */
+};
+
+
+
 void ft_putchar(char c);
 void ft_swap(int *a, int *b);
 void ft_putstr(char *str);
