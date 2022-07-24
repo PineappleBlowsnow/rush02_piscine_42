6fa071cb160a868e1a7c8f3641fe835f4976c586
coolcty <coolcty@qq.com>
2022-07-24 23:15:17 +0200

    update

--- srcs/main.c
+++ srcs/main.c
@@ -14,31 +14,37 @@ int ft_check_arg(int argc, char **argv, char **pnbr_str, int *pfd)
 		*pnbr_str = argv[1];
 	}
 	else
-	{
-		write(1, "Error\n", 6);
 		return (0);
-	}
+	if (!ft_isnumeric(*pnbr_str))
+		return (0);
 	*pfd = open(dict_name, O_RDONLY);
 	if ((*pfd) <= 0)
-	{
-		write(1, "Error\n", 6);
 		return (0);
-	}
 	return (1);
 }
-
+char *ft_trim_zero(nbr_str){
+	while((*nbr_str)=='0')
+		nbr_str++;
+	return nbr_str;
+}
 int main(int argc, char **argv)
 {
 	int fd;
 	char *nbr_str;
 	char *dict_str;
+	char *std_dict_str;
+	int nbr_size;
 
 	if (!ft_check_arg(argc, argv, &nbr_str, &fd))
+	{
+		write(1, "Error\n", 6);
 		return (-1);
+	}
+
 	dict_str = ft_read_data(fd);
 	close(fd);
+	nbr_str=ft_trim_zero(nbr_str);
+	nbr_size=ft_strlen(nbr_str);
 	
-
-
 	free(dict_str);
 }
