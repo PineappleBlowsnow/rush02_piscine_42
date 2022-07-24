e5a3520591de7bc62f3b27d963cfc45259e12652
coolcty <coolcty@qq.com>
2022-07-24 22:26:26 +0200

    update

--- srcs/main.c
+++ srcs/main.c
@@ -1,30 +1,40 @@
 #include "ft.h"
 
-int main(int argc, char **argv)
+int ft_check_arg(int argc, char **argv, char **pnbr_str, char **pdict_name)
 {
-    (void)argc;
-    (void)argv;
-
-    int fd;
-    t_dl dict_data;
-    t_dl cur;
-    char *nbr_str;
-    char *dict_name;
-    if (argc == 3)
+if (argc == 3)
     {
-        dict_name = argv[1];
-        nbr_str = argv[2];
+        *pdict_name = argv[1];
+        *pnbr_str = argv[2];
     }
     else if (argc == 2)
     {
-        dict_name = "numbers.dict.txt";
-        nbr_str = argv[1];
+        *pdict_name = "numbers.dict.txt";
+        *pnbr_str = argv[1];
     }
     else
     {
         write(1, "Error\n", 6);
+        return (0);
+    }
+    return (1);
+}
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    int fd;
+    char *nbr_str;
+    char *dict_name;
+    char *dict_str;
+    if (!ft_check_arg(argc, argv, &nbr_str, &dict_name))
+    {
+        write(1, "Error\n", 6);
         return (-1);
     }
+
+    
 
     char buf[BUFSIZE];
     fd = open(dict_name, O_RDONLY);
@@ -33,7 +43,8 @@ int main(int argc, char **argv)
         return (-1);
     }
 
-    read(fd, buf, BUFSIZE);
-
+    dict_str = ft_read_data(fd);
     close(fd);
+
+    free(dict_str);
 }
